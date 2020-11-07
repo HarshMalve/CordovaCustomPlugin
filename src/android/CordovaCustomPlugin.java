6e96ac8a5e2f74fe4b8d1337eed4a4cd41cd9603
Harshavardhan Malve <harsh@Harshavardhans-MacBook-Air.local>
2020-11-07 20:08:47 +0530

    initial commit

---
+++ src/android/CordovaCustomPlugin.java
@@ -0,0 +1,36 @@
+package info.androidabcd.plugin.custom;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class CordovaCustomPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            // String message = args.getString(0);
+            // this.coolMethod(message, callbackContext);
+            String arg1 = args.getString(0);
+            // int arg2 = args.getInt(1);
+            String result = arg1;
+            callbackContext.success('result: '+ result);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
